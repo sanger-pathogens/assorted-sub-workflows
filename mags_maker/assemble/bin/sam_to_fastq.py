2c44d3155f1e1c134559112f0901a5cec787d60a
William Roberts <wr7@sanger.ac.uk>
2025-10-04 17:38:06 +0100

    Add sam_to_fastq.py script for comparison

---
+++ mags_maker/assemble/bin/sam_to_fastq.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+for line in open(sys.argv[1]):
+	cut=line.split('\t')
+	if len(cut)<11: continue
+	print(">"+cut[0])
+	print(cut[9])
+	print("+")
+	print(cut[10])
