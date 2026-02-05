1ded4e58e38d0656e23055d0fd8e14d8292d8e38
William Roberts <wr7@sanger.ac.uk>
2026-02-05 09:31:06 +0000

    Use caching strategy to avoid reliance on read order in sam/bam

--- mags_maker/reassemble/modules/bin/filter_reads_for_bin_reassembly.py
+++ mags_maker/reassemble/modules/bin/filter_reads_for_bin_reassembly.py
@@ -49,6 +49,7 @@ def main():
                     contig_bins[line[1:-1]] = bin_name
 
     print("Parsing sam file and writing reads to appropriate files depending what bin they alligned to...")
+    # files to store output file handles for each bin and mismatch category (strict/permissive)
     files = {}
     opened_bins = {}
 
@@ -59,81 +60,97 @@ def main():
     else:
         input_stream = AlignmentFile("-", "r")
     
-    # Includes unmapped reads (probably don't want this)
-    # 
+    # Includes unmapped reads (probably don't want this, but also preserves read order)
     sam_iter = input_stream.fetch(until_eof=True)
 
+    # Cache to hold reads until their mates are found (allows processing mapped reads in one pass
+    # - memory efficient way to process mapped reads in any arbitary order)
+    read_cache = {}
+
     for read in sam_iter:
-        line = read.to_string()
-        cut = line.strip().split("\t")
-        binary_flag = bin(int(cut[1]))
-
-        if binary_flag[-7] == "1":
-            F_line = line
+        # Ignore unpaired, secondary, and supplementary alignments
+        if not read.is_paired:
             continue
-        elif binary_flag[-8] == "1":
-            R_line = line
+        if read.is_secondary or read.is_supplementary:
+            continue
 
-            F_cut = F_line.strip().split("\t")
-            R_cut = R_line.strip().split("\t")
+        qname = read.query_name
 
-            if F_cut[2] == "*" and R_cut[2] == "*":
+        if qname in read_cache:
+            mate = read_cache.pop(qname)
+
+            F_line = read.to_string() if read.is_read1 else mate.to_string()
+            R_line = read.to_string() if read.is_read2 else mate.to_string()
+        else:
+            read_cache[qname] = read
+            continue
+
+        # Start processing read pair
+        F_cut = F_line.strip().split("\t")
+        R_cut = R_line.strip().split("\t")
+
+        # Check if unmapped (although SAM spec also allows a coordinate here for unmapped reads, even if meaningless/only to allow sorting)
+        if F_cut[2] == "*" and R_cut[2] == "*":
+            continue
+
+        if F_cut[2] != R_cut[2]:
+            # If each segment in read pair mapped to different contigs and either contig isn't included in bins, the skip.
+            if F_cut[2] not in contig_bins or R_cut[2] not in contig_bins:
                 continue
-
-            if F_cut[2] != R_cut[2]:
-                if F_cut[2] not in contig_bins or R_cut[2] not in contig_bins:
-                    continue
-                bin1 = contig_bins[F_cut[2]]
-                bin2 = contig_bins[R_cut[2]]
-                if bin1 != bin2:
-                    continue
-                bin_name = bin1
-            else:
-                contig = F_cut[2]
-                if contig not in contig_bins:
-                    continue
-                bin_name = contig_bins[contig]
-
-            if "NM:i:" not in F_line and "NM:i:" not in R_line:
+            bin1 = contig_bins[F_cut[2]]
+            bin2 = contig_bins[R_cut[2]]
+            if bin1 != bin2:
+                # If the different contig to which read segments mapped aren't in the same bin, skip.
                 continue
+            bin_name = bin1
+        else:
+            contig = F_cut[2]
+            if contig not in contig_bins:
+                # If the contig to which both read segments mapped aren't in the same bin, skip.
+                continue
+            bin_name = contig_bins[contig]
 
-            if bin_name not in opened_bins:
-                opened_bins[bin_name] = None
-                for mode in ["strict", "permissive"]:
-                    for end in [1, 2]:
-                        filename = os.path.join(args.output_dir, f"{bin_name}.{mode}_{end}.fastq.gz")
-                        files[filename] = gzip.open(filename, "wt")
+        # Check if both reads are unmapped (again)
+        if "NM:i:" not in F_line and "NM:i:" not in R_line:
+            continue
 
-            cumulative_mismatches = 0
-            for field in F_cut:
-                if field.startswith("NM:i:"):
-                    cumulative_mismatches += int(field.split(":")[-1])
-                    break
-            for field in R_cut:
-                if field.startswith("NM:i:"):
-                    cumulative_mismatches += int(field.split(":")[-1])
-                    break
+        if bin_name not in opened_bins:
+            opened_bins[bin_name] = None
+            for mode in ["strict", "permissive"]:
+                for end in [1, 2]:
+                    filename = os.path.join(args.output_dir, f"{bin_name}.{mode}_{end}.fastq.gz")
+                    files[filename] = gzip.open(filename, "wt")
 
-            F_binary_flag = bin(int(F_cut[1]))
-            R_binary_flag = bin(int(R_cut[1]))
+        cumulative_mismatches = 0
+        for field in F_cut:
+            if field.startswith("NM:i:"):
+                cumulative_mismatches += int(field.split(":")[-1])
+                break
+        for field in R_cut:
+            if field.startswith("NM:i:"):
+                cumulative_mismatches += int(field.split(":")[-1])
+                break
 
-            if F_binary_flag[-5] == '1':
-                F_cut[9] = rev_comp(F_cut[9])
-                F_cut[10] = F_cut[10][::-1]
-            if R_binary_flag[-5] == '1':
-                R_cut[9] = rev_comp(R_cut[9])
-                R_cut[10] = R_cut[10][::-1]
+        F_binary_flag = bin(int(F_cut[1]))
+        R_binary_flag = bin(int(R_cut[1]))
 
-            if cumulative_mismatches < strict_snp_cutoff:
-                files[os.path.join(args.output_dir, f"{bin_name}.strict_1.fastq.gz")].write(f"@{F_cut[0]}/1\n{F_cut[9]}\n+\n{F_cut[10]}\n")
-                files[os.path.join(args.output_dir, f"{bin_name}.strict_2.fastq.gz")].write(f"@{R_cut[0]}/2\n{R_cut[9]}\n+\n{R_cut[10]}\n")
+        if F_binary_flag[-5] == '1':
+            F_cut[9] = rev_comp(F_cut[9])
+            F_cut[10] = F_cut[10][::-1]
+        if R_binary_flag[-5] == '1':
+            R_cut[9] = rev_comp(R_cut[9])
+            R_cut[10] = R_cut[10][::-1]
 
-            if cumulative_mismatches < permissive_snp_cutoff:
-                files[os.path.join(args.output_dir, f"{bin_name}.permissive_1.fastq.gz")].write(f"@{F_cut[0]}/1\n{F_cut[9]}\n+\n{F_cut[10]}\n")
-                files[os.path.join(args.output_dir, f"{bin_name}.permissive_2.fastq.gz")].write(f"@{R_cut[0]}/2\n{R_cut[9]}\n+\n{R_cut[10]}\n")
+        if cumulative_mismatches < strict_snp_cutoff:
+            files[os.path.join(args.output_dir, f"{bin_name}.strict_1.fastq.gz")].write(f"@{F_cut[0]}/1\n{F_cut[9]}\n+\n{F_cut[10]}\n")
+            files[os.path.join(args.output_dir, f"{bin_name}.strict_2.fastq.gz")].write(f"@{R_cut[0]}/2\n{R_cut[9]}\n+\n{R_cut[10]}\n")
+
+        if cumulative_mismatches < permissive_snp_cutoff:
+            files[os.path.join(args.output_dir, f"{bin_name}.permissive_1.fastq.gz")].write(f"@{F_cut[0]}/1\n{F_cut[9]}\n+\n{F_cut[10]}\n")
+            files[os.path.join(args.output_dir, f"{bin_name}.permissive_2.fastq.gz")].write(f"@{R_cut[0]}/2\n{R_cut[9]}\n+\n{R_cut[10]}\n")
 
     print("closing files")
-    if args.sam:
+    if args.mapped_reads:
         input_stream.close()
         
     for f in files.values():
